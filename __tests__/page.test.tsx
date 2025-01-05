9872234ef872ac51af15c16ce0d3e796cb267a76
Huncho <hunchodotdev@gmail.com>
2025-01-05 02:28:43 +0000

    add demo test

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -1,35 +1,40 @@
-import { render, screen } from '@testing-library/react'
-import Home from '../app/page'
-import '@testing-library/jest-dom'
+import { render, screen } from "@testing-library/react";
 
-describe('Home Page', () => {
-  it('renders the Next.js logo', () => {
-    render(<Home />)
-    const logo = screen.getByAltText('Next.js logo')
-    expect(logo).toBeInTheDocument()
-  })
+import Home from "../app/page";
 
-  it('renders the getting started instructions', () => {
-    render(<Home />)
-    const instructions = screen.getByText(/Get started by editing/)
-    expect(instructions).toBeInTheDocument()
-  })
+import "@testing-library/jest-dom";
 
-  it('renders the deploy button', () => {
-    render(<Home />)
-    const deployButton = screen.getByText('Deploy now')
-    expect(deployButton).toBeInTheDocument()
-    expect(deployButton).toHaveAttribute('href', 'https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app')
-  })
+describe("Home Page", () => {
+  it("renders the Next.js logo", () => {
+    render(<Home />);
+    const logo = screen.getByAltText("Next.js logo");
+    expect(logo).toBeInTheDocument();
+  });
 
-  it('renders the footer links', () => {
-    render(<Home />)
-    const learnLink = screen.getByText('Learn')
-    const examplesLink = screen.getByText('Examples')
-    const docsLink = screen.getByText(/Go to nextjs.org/)
+  it("renders the getting started instructions", () => {
+    render(<Home />);
+    const instructions = screen.getByText(/Get started by editing/);
+    expect(instructions).toBeInTheDocument();
+  });
 
-    expect(learnLink).toBeInTheDocument()
-    expect(examplesLink).toBeInTheDocument()
-    expect(docsLink).toBeInTheDocument()
-  })
-})
+  it("renders the deploy button", () => {
+    render(<Home />);
+    const deployButton = screen.getByText("Deploy now");
+    expect(deployButton).toBeInTheDocument();
+    expect(deployButton).toHaveAttribute(
+      "href",
+      "https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    );
+  });
+
+  it("renders the footer links", () => {
+    render(<Home />);
+    const learnLink = screen.getByText("Learn");
+    const examplesLink = screen.getByText("Examples");
+    const docsLink = screen.getByText(/Go to nextjs.org/);
+
+    expect(learnLink).toBeInTheDocument();
+    expect(examplesLink).toBeInTheDocument();
+    expect(docsLink).toBeInTheDocument();
+  });
+});
